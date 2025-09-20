bab9fe35b05c1361f667780171ef7722be219a9a
Ruben Pulido <77760285+rubenpulido1@users.noreply.github.com>
2025-09-19 17:42:06 -0700

    Modified reports and created Angular project along with registering HTTP client globally

--- FullStack-Training/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ FullStack-Training/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76c3698c51c2cc610a258cf2968143073a0630ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95e8768d2d5c4fc449873dda72269d5882d43309")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
