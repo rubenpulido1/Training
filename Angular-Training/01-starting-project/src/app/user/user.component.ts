8e9f36b0e94b494802f80c9ae530a81bf2777d94
Ruben Pulido <77760285+rubenpulido1@users.noreply.github.com>
2025-09-30 11:46:10 -0700

    Using output function instead of decorator

--- Angular-Training/01-starting-project/src/app/user/user.component.ts
+++ Angular-Training/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, signal, input, Output, EventEmitter } from '@angular/core';
+import { Component, computed, Input, signal, input, Output, EventEmitter, output } from '@angular/core';
 // importing DUMMY USERS array
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -15,8 +15,9 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
-  @Output() select = new EventEmitter();
-
+  // @Output() select = new EventEmitter();
+  select = output<string>();
+  
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
