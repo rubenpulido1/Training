bdfa77a5fd8c984a7042328d0df7bd35a47abcd8
Ruben Pulido <77760285+rubenpulido1@users.noreply.github.com>
2025-09-30 09:04:53 -0700

    Introducing Signals

--- FullStack-Training/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ FullStack-Training/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+297870e496b219b7a7b69bc6647768cb6722e8c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1062fc80fbd494fa517865906a776e57ac23cabf")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
