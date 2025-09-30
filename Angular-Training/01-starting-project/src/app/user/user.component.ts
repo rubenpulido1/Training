831f49d1d2dbb508aff4206adb192ada99242f3c
Ruben Pulido <77760285+rubenpulido1@users.noreply.github.com>
2025-09-30 10:22:26 -0700

    Used property binding to bind to custom properties Parent -> Child

--- Angular-Training/01-starting-project/src/app/user/user.component.ts
+++ Angular-Training/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, Input, signal, input } from '@angular/core';
 // importing DUMMY USERS array
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -12,15 +12,12 @@ const randomIndex =  Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
 
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser().avatar;
-  // }
-
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
   onSelectUser() {
-    const randomIndex =  Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
