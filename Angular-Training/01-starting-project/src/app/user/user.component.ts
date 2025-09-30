48370542c4c597d180a6c32616a7a812678bcb6c
Ruben Pulido <77760285+rubenpulido1@users.noreply.github.com>
2025-09-30 10:48:24 -0700

    Using signal inputs

--- Angular-Training/01-starting-project/src/app/user/user.component.ts
+++ Angular-Training/01-starting-project/src/app/user/user.component.ts
@@ -12,12 +12,14 @@ const randomIndex =  Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  avatar = input.required<string>();
+  name = input.required<string>();
+  imagePath = computed(() => 'assets/users/' + this.avatar());
 
-  get imagePath() {
-    return 'assets/users/' + this.avatar;
-  }
+  // get imagePath() {
+  //   return 'assets/users/' + this.avatar();
+  // }
+
   onSelectUser() {
   }
 }
