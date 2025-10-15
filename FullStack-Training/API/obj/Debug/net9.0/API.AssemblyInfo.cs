09513261a290750f8352e4d9a28fa1180f711a96
Ruben Pulido <77760285+rubenpulido1@users.noreply.github.com>
2025-10-15 15:31:05 -0700

    Updating Styling for reports

--- FullStack-Training/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ FullStack-Training/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dfd9416f01334bf699e392669e64acc43939fa2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47a4517890a9db738ff38f9fb5a58ce112559f87")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
