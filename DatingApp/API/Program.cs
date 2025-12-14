9d0f088f01019cfadcc01f16a17426f65d922244
Ruben Pulido <77760285+rubenpulido1@users.noreply.github.com>
2025-12-13 16:11:12 -0800

    End of Section 3 - Angular Skeleton

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -12,9 +12,13 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
   opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+  .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
